939bd12a6f141de32990f7d56f8a129128f872af
Sergio <ssegura@op2aim.io>
2022-02-13 21:38:36 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ export default function App() {
       <Routes>
         {getRoutes(routes)}
         <Route path="/" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
   );
