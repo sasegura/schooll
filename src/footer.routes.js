82503af4f9e45dcea3505b960994ae965670d936
Sergio <ssegura@op2aim.io>
2022-02-19 20:02:46 +0100

    dasd

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -1,9 +1,5 @@
 import React from 'react';
 // @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import YouTubeIcon from "@mui/icons-material/YouTube";
 
 // Material Kit 2 React components
 import MKTypography from "components/MKTypography";
